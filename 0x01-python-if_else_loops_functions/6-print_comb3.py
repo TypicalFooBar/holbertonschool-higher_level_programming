eea92f8a8300e2f4710e82a3234193981c92364e
Derek Webb <derek.webb@holbertonschool.com>
2020-03-08 23:03:53 +0000

    hlp.1.6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+usedCombinations = [[0 for i in range(10)] for j in range(10)]
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i == j:
+            usedCombinations[i][j] = 1
+            usedCombinations[j][i] = 1
+            continue
+
+        if usedCombinations[i][j] == 1 or usedCombinations[j][i] == 1:
+            continue
+
+        print("{}{}".format(i, j), end='')
+
+        # Mark this combination as used
+        usedCombinations[i][j] = 1
+        usedCombinations[j][i] = 1
+
+        if i == 8 and j == 9:
+            print("")
+            break
+        
+        # Print a comma
+        print(", ", end='')
