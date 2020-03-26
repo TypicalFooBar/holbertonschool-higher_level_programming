b94f91d799d5389ea7b45e6444e02b5477d11e0b
Derek Webb <derek.webb@holbertonschool.com>
2020-03-26 22:40:00 +0000

    hlp.3.10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    listLength = len(my_list)
+    divisibleBy2List = [False] * listLength
+
+    for i in range(listLength):
+        if my_list[i] %2 is 0:
+            divisibleBy2List[i] = True
+
+    return divisibleBy2List
