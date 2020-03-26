2b9e9377f7f2afd01166e059106a7916050b7e1b
Derek Webb <derek.webb@holbertonschool.com>
2020-03-26 20:17:44 +0000

    hlp.3.4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        copy = my_list.copy()
+        return copy
+    else:
+        copy = my_list.copy()
+        copy[idx] = element
+        return copy
