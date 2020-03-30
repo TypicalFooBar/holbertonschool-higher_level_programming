142ab93e0381b5d1fa93a5f4b1f8985af7e59a4e
Derek Webb <derek.webb@holbertonschool.com>
2020-03-30 22:34:41 +0000

    hlp.4.0

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-def square_matrix_simple(matrix=[]):
+def search_replace(my_list, search, replace):
     newMatrix = [row[:] for row in matrix]
 
-    for row in range(len(newMatrix)):
-        for col in range(len(newMatrix[row])):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
             newMatrix[row][col] = newMatrix[row][col] ** 2
 
     return newMatrix
