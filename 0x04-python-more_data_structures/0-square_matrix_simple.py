402e444a823400d0102b9666ae51ba1ab40c9741
Derek Webb <derek.webb@holbertonschool.com>
2020-03-30 22:48:52 +0000

    hlp.0.0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,13 @@
 
 
 def square_matrix_simple(matrix=[]):
-    newMatrix = [row[:] for row in matrix]
+    # newMatrix = [row[:] for row in matrix]
 
-    for row in range(len(newMatrix)):
-        for col in range(len(newMatrix[row])):
-            newMatrix[row][col] = newMatrix[row][col] ** 2
+    # for row in range(len(newMatrix)):
+    #     for col in range(len(newMatrix[row])):
+    #         newMatrix[row][col] = newMatrix[row][col] ** 2
 
+    # return newMatrix
+
+    newMatrix = [[col**2 for col in row] for row in matrix]
     return newMatrix
