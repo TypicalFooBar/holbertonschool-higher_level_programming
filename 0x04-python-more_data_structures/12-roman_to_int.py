07ca1537c2ef8ddadc88b1fd2ddbb9863524c915
Derek Webb <derek.webb@holbertonschool.com>
2020-03-31 01:21:26 +0000

    hlp.4.12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     sum = 0
-    lastCharWasI = False
+    lastRoman = None
 
     if isinstance(roman_string, str):
         for c in roman_string:
             if c is 'I':
                 sum += 1
-                lastCharWasI = True
             elif c is 'V':
-                if lastCharWasI:
+                if lastRoman is 'I':
                     sum += 3
                 else:
                     sum += 5
-
-                lastCharWasI = False
             elif c is 'X':
-                if lastCharWasI:
+                if lastRoman is 'I':
                     sum += 8
                 else:
                     sum += 10
-
-                lastCharWasI = False
             elif c is 'L':
                 sum += 50
-                lastCharWasI = False
             elif c is 'C':
-                sum += 100
-                lastCharWasI = False
+                if lastRoman is 'X':
+                    sum += 80
+                else:
+                    sum += 100
             elif c is 'D':
                 sum += 500
-                lastCharWasI = False
+            
+            lastRoman = c
 
     return sum
