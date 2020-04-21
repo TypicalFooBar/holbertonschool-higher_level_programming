8bdf0e564fc3c07e4e50ba1d0cffcb5480f74e39
Derek Webb <derek.webb@holbertonschool.com>
2020-04-21 23:28:32 +0000

    hlp.6.9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+class Square:
+    __size = 0
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if value is not None:
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+    def __lt__(self, other):
+        return self.size < other.size
+
+    def __gt__(self, other):
+        return self.size > other.size
+
+    def __le__(self, other):
+        return self.size <= other.size
+
+    def __ge__(self, other):
+        return self.size >= other.size
+
+    def __eq__(self, other):
+        return self.size == other.size
+
+    def __ne__(self, other):
+        return self.size != other.size
