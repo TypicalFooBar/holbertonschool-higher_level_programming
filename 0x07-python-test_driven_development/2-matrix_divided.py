4b99123159ac2b561c295e221ac212ea8177a416
Derek Webb <derek.webb@holbertonschool.com>
2020-04-22 16:27:31 +0000

    hlp.7.1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,26 +2,35 @@
 
 
 def validateMatrixAndGetRowSize(matrix):
-    nonListMessage = "matrix must be a matrix (list of lists) of integers/floats"
-    wrongSizeRowMessage = "Each row of the matrix must have the same size"
+    nonList = "matrix must be a matrix (list of lists) of integers/floats"
+    wrongSizeRow = "Each row of the matrix must have the same size"
     rowSize = -1
 
     if (
         not isinstance(matrix, list)
     ):
-        raise TypeError(nonListMessage)
+        raise TypeError(nonList)
     else:
         for row in matrix:
             # Make sure this is a row
             if not isinstance(row, list):
-                raise TypeError(nonListMessage)
+                raise TypeError(nonList)
 
             # Get the length of the row
             if rowSize == -1:
                 rowSize = len(row)
             # Make sure all rows are the same length
             elif len(row) != rowSize:
-                raise TypeError(wrongSizeRowMessage)
+                raise TypeError(wrongSizeRow)
+
+            # Make sure each element is an int or float
+            for col in row:
+                if (
+                    not isinstance(col, int) and
+                    not isinstance(col, float)
+                ):
+                    raise TypeError(nonList)
+
 
 def validateDivisor(div):
     if (
@@ -31,6 +40,7 @@ def validateDivisor(div):
         raise TypeError("div must be a number")
     elif div == 0:
         raise TypeError("division by zero")
+
 
 def matrix_divided(matrix, div):
     validateDivisor(div)
