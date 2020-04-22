e7ccde29b7d7515949cebf4bc878da09093a1c5a
Derek Webb <derek.webb@holbertonschool.com>
2020-04-22 17:08:34 +0000

    hlp.7.4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,6 +6,16 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     text = text.replace(".", ".\n\n").replace("?", "?\n\n")
-    text = text.replace(":", ":\n\n").replace("\n\n ", "\n\n")
+    text = text.replace(":", ":\n\n")
+
+    # Remove all extra white space after "\n\n"
+    while True:
+        newText = text.replace("\n\n ", "\n\n")
+
+        # If no change was made, then we're done
+        if (newText == text):
+            break
+        else:
+            text = newText
 
     print(text, end="")
