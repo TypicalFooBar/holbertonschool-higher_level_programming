3ae4379d94b3fac83056bfab1890bf6642269430
Derek Webb <derek.webb@holbertonschool.com>
2020-05-03 23:35:59 +0000

    hlp.0a.13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 
-def add_attribute(myClass, key, value):
-    setattr(myClass, key, value)
+def add_attribute(obj, key, value):
+    setattr(obj, key, value)
+    if not hasattr(obj, key):
+        raise TypeError("can't add new attribute")
