0abd07c4cae4e436ebf3bce386861874e4e8dd79
Derek Webb <derek.webb@holbertonschool.com>
2020-05-03 23:01:03 +0000

    hlp.0a.12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,7 @@ class MyInt(int):
     """MyInt"""
 
     def __eq__(self, other):
-        return self != other
+        return other.__ne__(self)
 
     def __ne__(self, other):
-        return self == other
+        return other.__eq__(self)
