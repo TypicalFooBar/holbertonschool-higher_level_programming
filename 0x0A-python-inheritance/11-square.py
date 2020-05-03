94f1e58c989ddf7ea702d2ed177c717877752393
Derek Webb <derek.webb@holbertonschool.com>
2020-05-03 22:48:36 +0000

    hlp.0a.11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+
+        self._size = size
+
+    def area(self):
+        return self._size * self._size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self._size, self._size)
+
+    def __repr__(self):
+        return self.__str__()
