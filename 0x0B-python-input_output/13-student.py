c8a37b66ef284d363e7254de9bb6be735b0f51fe
Derek Webb <derek.webb@holbertonschool.com>
2020-05-05 22:08:06 +0000

    hlp.0b.13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+class Student:
+    first_name = ""
+    last_name = ""
+    age = ""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            studentAttrs = {}
+            for item in attrs:
+                try:
+                    studentAttrs[item] = self.__dict__[item]
+                except KeyError:
+                    # Do nothing if the key did not exist
+                    pass
+
+            return studentAttrs
+
+    def reload_from_json(self, json):
+        for item in json:
+            try:
+                self.__dict__[item] = json[item]
+            except KeyError:
+                # Do nothing if the key did not exist
+                pass
