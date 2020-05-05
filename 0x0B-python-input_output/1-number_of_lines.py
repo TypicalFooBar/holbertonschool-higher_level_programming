d4e795ed908d76f6eb95336875d674bacdee1575
Derek Webb <derek.webb@holbertonschool.com>
2020-05-05 20:35:36 +0000

    hlp.0b.1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,7 +5,13 @@ def number_of_lines(filename=""):
     i = 0
 
     with open(filename) as f:
+        atLeastOneLine = False
+        
         for i, line in enumerate(f):
-            pass
+            atLeastOneLine = True
+
+        # Account for index starting at zero
+        if atLeastOneLine:
+            i += 1
 
     return i
