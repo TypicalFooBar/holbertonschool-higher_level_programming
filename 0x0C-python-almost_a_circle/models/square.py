1e85c5724685568ed74a1de882b0ce4f827941df
Derek Webb <derek.webb@holbertonschool.com>
2020-05-09 17:15:41 +0000

    hlp.0c.14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -52,3 +52,12 @@ class Square(Rectangle):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Description"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
