8a82b37d2b127dd8ff53c1b35c3ec147d6a4a35f
Derek Webb <derek.webb@holbertonschool.com>
2021-02-06 19:09:15 +0000

    hlp.12.3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Count the arguments
+let count = -2;
+process.argv.forEach((val, index) => {
+	count++;
+
+	if (count > 0)
+		console.log(val);
+});
+
+if (count == 0)
+	console.log('No argument');
