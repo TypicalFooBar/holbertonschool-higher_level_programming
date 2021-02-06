6c9ea4e75b1273870c01d9c30756e4e75823c3c0
Derek Webb <derek.webb@holbertonschool.com>
2021-02-06 23:36:09 +0000

    hlp.13.13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,7 +6,7 @@ try {
 	const fileA = fs.readFileSync(process.argv[2], 'utf8')
 	const fileB = fs.readFileSync(process.argv[3], 'utf8')
 
-	fs.writeFileSync(process.argv[4], fileA + "\n" + fileB)
+	fs.writeFileSync(process.argv[4], fileA + fileB)
 } catch (err) {
 	console.error(err)
 }
