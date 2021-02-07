3da406991657a7e1b4af7f3de8c90b28beef07be
Derek Webb <derek.webb@holbertonschool.com>
2021-02-07 00:18:42 +0000

    hlp.14.4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request')
 
 let count = 0
 
-request(`https://swapi-api.hbtn.io/api/films`, (error, response, body) => {
+request(process.argv[2], (error, response, body) => {
 	let data = JSON.parse(body)
 	data.results.forEach(film => {
 		if (film.characters.includes("https://swapi-api.hbtn.io/api/people/18/"))
