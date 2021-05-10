66d05649e294b236bd457f0ba0061bf2fc08c72c
Derek Webb <derek.webb@holbertonschool.com>
2021-05-10 20:56:42 +0000

    100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(h="Holberton"):
-    magic_string.s=magic_string.s+', '+h if hasattr(magic_string, "s") else h
+    magic_string.s=magic_string.s + ', '+h if hasattr(magic_string, "s") else h
     return magic_string.s
