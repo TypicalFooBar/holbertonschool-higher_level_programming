8fd68b0a7bba6080cdbd37fef2b389158ec65cbd
Derek Webb <derek.webb@holbertonschool.com>
2021-05-10 19:54:36 +0000

    Magic String function

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,4 +2,3 @@
 def magic_string():
     magic_string.returnString = magic_string.returnString = magic_string.returnString + ", Holberton" if hasattr(magic_string, "returnString") else "Holberton"
     return magic_string.returnString
-    
