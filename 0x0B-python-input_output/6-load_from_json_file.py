b6f9bc2c172c41c325a715845f99d9f5f9101642
Derek Webb <derek.webb@holbertonschool.com>
2021-05-12 19:28:16 +0000

    load from json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import json
+""" Module doc """
+
 
 def load_from_json_file(filename):
+    """ Function doc"""
+
     with open(filename, 'r') as file:
         jsonObject = json.load(file)
         return jsonObject
